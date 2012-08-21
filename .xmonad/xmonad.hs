e85263c4ec7cabd4ad4ec829f3a419033396d153
Raitis Stengrevics <raitis.stengrevics@eglobal-forex.com>
2012-08-21 12:46:39 +0300

    Added .xmonad/xmobar.hs

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig
+import XMonad.Prompt
+import XMonad.Prompt.RunOrRaise
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
+
+main = do
+    conf <- dzen defaultConfig
+    xmonad $ conf
+        {
+          terminal = "urxvt"
+        , layoutHook                = smartBorders (layoutHook conf)
+        , XMonad.borderWidth        = 4
+        , XMonad.normalBorderColor  = "black"
+        , XMonad.focusedBorderColor = "#1793d1"
+        }
+         `additionalKeysP`
+        [
+          ("M-p",         spawn "`yeganesh -x -- -i -nb black -nf white -sb \"#1793d1\" -sf white`")
+        , ("<Print>",     spawn "scrot '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Screenshots/'")
+        , ("M-S-<Print>", spawn "scrot '%Y-%m-%d_$wx$h.png' -s -e 'mv $f ~/Screenshots/'") -- Doesn't work!
+        , ("M-S-l",       spawn "xscreensaver-command -lock")
+        ]
