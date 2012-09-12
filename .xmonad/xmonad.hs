f19c1c3c5c44f7bb921b29befb186e6f4eab979f
Raitis Stengrevics <raitis.stengrevics@eglobal-forex.com>
2012-09-12 12:38:38 +0300

    Added schortcut for ST2 and a little restructured conf for Xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,14 +18,15 @@ main = do
         , XMonad.normalBorderColor  = "black"
         , XMonad.focusedBorderColor = "#1793d1"
         }
-         `additionalKeysP`
+        `additionalKeysP`
         [
           ("M-S-<Return>", spawn "urxvt -lsp 2 -bc")
+        , ("M-S-a",        spawn "subl -n")
+        , ("M-S-i",        spawn "chromium")
+        , ("M-C-i",        spawn "chromium --incognito")
+        , ("M-C-<Delete>", spawn "urxvt -e glances")
         , ("M-p",          spawn "`yeganesh -x -- -i -nb black -nf white -sb \"#1793d1\" -sf white`")
         , ("<Print>",      spawn "scrot '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Screenshots/'")
         , ("M-S-<Print>",  spawn "scrot '%Y-%m-%d_$wx$h.png' -s -e 'mv $f ~/Screenshots/'") -- Doesn't work!
         , ("M-S-l",        spawn "xscreensaver-command -lock")
-        , ("M-S-i",        spawn "chromium")
-        , ("M-C-i",        spawn "chromium --incognito")
-        , ("M-C-<Delete>", spawn "urxvt -e glances")
         ]
