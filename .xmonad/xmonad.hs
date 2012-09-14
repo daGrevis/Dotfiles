c4a8dd6b80c0a6256f7621eaaa85d709497a22d0
Raitis Stengrevics <raitis.stengrevics@eglobal-forex.com>
2012-09-14 12:36:01 +0300

    DRY for opening stuff in terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,11 +8,14 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
 
+my_terminal = "urxvt -lsp 2 -bc"
+term = \s -> spawn $ my_terminal++" -e "++s
+
 main = do
     conf <- dzen defaultConfig
     xmonad $ conf
         {
-          terminal = "urxvt"
+          terminal = my_terminal
         , layoutHook                = smartBorders (layoutHook conf)
         , XMonad.borderWidth        = 4
         , XMonad.normalBorderColor  = "black"
@@ -20,12 +23,11 @@ main = do
         }
         `additionalKeysP`
         [
-          ("M-S-<Return>", spawn "urxvt -lsp 2 -bc")
-        , ("M-S-a",        spawn "subl -n")
-        , ("M-S-v",        spawn "urxvt -lsp 2 -bc -e vim")
+          ("M-S-a",        spawn "subl -n")
+        , ("M-S-v",        term  "vim")
         , ("M-S-i",        spawn "chromium")
         , ("M-C-i",        spawn "chromium --incognito")
-        , ("M-C-<Delete>", spawn "urxvt -lsp 2 -bc -e glances")
+        , ("M-C-<Delete>", term  "glances")
         , ("M-p",          spawn "`yeganesh -x -- -i -nb black -nf white -sb \"#1793d1\" -sf white`")
         , ("<Print>",      spawn "scrot '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Screenshots/'")
         , ("M-S-<Print>",  spawn "scrot '%Y-%m-%d_$wx$h.png' -s -e 'mv $f ~/Screenshots/'") -- Doesn't work!
