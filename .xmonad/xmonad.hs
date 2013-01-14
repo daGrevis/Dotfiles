266a51f661efec79f2c08560a2c44ded3f1c8830
Raitis Stengrevics <dagrevis@gmail.com>
2013-01-15 00:40:41 +0200

    Add script and binding for toggling trackpad.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -34,4 +34,5 @@ main = do
         , ("M-C-i", spawn "chromium --incognito") -- Opens browser in incognito mode.
         , ("M-S-s", spawn "skype") -- Opens IM-client.
         , ("M-S-x", spawn "xchat") -- Opens IRC-client.
+        , ("M-S-t", spawn "trackpad-toggle.sh") -- Toggles trackpad.
         ]
