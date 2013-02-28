cc0ae1ece22b41e7ff5ce59ce4f03afe5f0850af
Raitis Stengrevics <raitis.stengrevics@eglobal-forex.com>
2013-02-28 11:50:01 +0200

    Added bundles for Vim as Git submodules and splitted script for Vim so there are two scripts: for creating dirs and for installing Pathogen.

---
+++ create_dirs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR='~/.local/share/vim/'
+mkdir -p $DIR
+mkdir $DIR'swap/'
+mkdir $DIR'backup/'
+mkdir $DIR'undo/'
---
+++ install_pathogen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
