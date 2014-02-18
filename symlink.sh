05f90133e250dacad1a1940f483e38c5e75830cd
Raitis Stengrevics <dagrevis@gmail.com>
2014-02-18 18:52:21 +0200

    Add symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ln -s ~/Dotfiles/coffeelint.json ~/coffeelint.json
+ln -s ~/Dotfiles/Custom.css ~/.config/chromium/Default/User\ StyleSheets/Custom.css
+ln -s ~/Dotfiles/custom.zsh-theme ~/.oh-my-zsh/themes/custom.zsh-theme
+ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
+ln -s ~/Dotfiles/profiles.clj ~/.lein/profiles.clj
+ln -s ~/Dotfiles/pylama.ini ~/pylama.ini
+ln -s ~/Dotfiles/toggle_trackpad.sh ~/Scripts/toggle_trackpad.sh
+ln -s ~/Dotfiles/user.behaviors ~/.config/LightTable/settings/user.behaviors
+ln -s ~/Dotfiles/.vimrc ~/.vimrc
+ln -s ~/Dotfiles/.xinitrc ~/.xinitrc
+ln -s ~/Dotfiles/.xmobarrc ~/.xmobarrc
+ln -s ~/Dotfiles/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/Dotfiles/.Xresources ~/.Xresources
+ln -s ~/Dotfiles/.zshenv ~/.zshenv
+ln -s ~/Dotfiles/.zshrc ~/.zshrc
