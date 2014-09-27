9d858048f026b5354f5629b31b70dc7c56bd0424
Raitis Stengrevics <dagrevis@gmail.com>
2014-09-27 12:36:28 +0300

    Fix location of infobar for case with one desktop

--- rc.lua
+++ rc.lua
@@ -217,7 +217,7 @@ for s = 1, screen.count() do
 
     local right_layout = wibox.layout.fixed.horizontal()
 
-    if s == 2 then
+    if screen.count() == 1 or s == 2 then
 
         right_layout:add(textclock_widget)
 
