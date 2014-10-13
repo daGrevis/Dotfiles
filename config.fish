3dbd657a1798c986472d281a3190ea95104d0811
Raitis Stengrevics <dagrevis@gmail.com>
2014-10-13 11:20:03 +0300

    Add cd function

--- config.fish
+++ config.fish
@@ -7,11 +7,17 @@ set fish_plugins archlinux autojump
 . $fish_path/oh-my-fish.fish
 
 alias l='ls -lahtr'
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias -="cd -"
+function cd
+    if count $argv >/dev/null
+        if test $argv = "..."
+            builtin cd ../../
+        else
+            builtin cd $argv
+        end
+    else
+        builtin cd
+    end
+end
 function mkcd
     mkdir -p $argv
     cd $argv
