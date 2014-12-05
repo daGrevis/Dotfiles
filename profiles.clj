aee695d2db770d00a46226a92cc798fb1886574b
Raitis Stengrevics <dagrevis@gmail.com>
2014-12-05 23:44:40 +0200

    Add criterium to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-exec "0.3.4"]
-                  [lein-midje "3.1.1"]]}}
+                  [lein-midje "3.1.1"]
+                  [criterium "0.4.3"]]}}
 (use '[leiningen.exec :only (deps)])
