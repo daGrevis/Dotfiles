cd3fe50c7413fe26dd971b1dfbe5172e26f3cee6
Raitis Stengrevics <dagrevis@gmail.com>
2015-01-12 17:57:59 +0200

    Add notification on locking screen

--- lock.sh
+++ lock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+notify-send 'Locking screen' 'Right now'
 scrot -q 100 /tmp/screenshot.png
 convert /tmp/screenshot.png -blur 0x07 /tmp/screenshotblur.png
 i3lock -u -i /tmp/screenshotblur.png
