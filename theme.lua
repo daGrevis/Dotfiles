10dbe791a035dcfe9b59cc0f3f59fa27e4e73025
Raitis Stengrevics <dagrevis@gmail.com>
2015-01-23 18:11:45 +0200

    Increase width of border for AwesomeWM borders

--- theme.lua
+++ theme.lua
@@ -17,7 +17,7 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.border_width  = 2
+theme.border_width  = 3
 theme.border_normal = "#000000"
 theme.border_focus  = "#1793d1"
 theme.border_marked = "#91231c"
