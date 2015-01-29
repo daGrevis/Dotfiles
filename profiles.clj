df01be1a619786395e8e9a62b5cc5acbdda33d90
Raitis Stengrevics <dagrevis@gmail.com>
2015-01-29 09:52:48 +0200

    Add Ultra for Lein

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[lein-exec "0.3.4"]
                   [lein-midje "3.1.1"]
-                  [criterium "0.4.3"]]}}
+                  [criterium "0.4.3"]
+                  [venantius/ultra "0.1.9"]]
+        :ultra {:color-scheme :solarized_dark}}}
 (use '[leiningen.exec :only (deps)])
