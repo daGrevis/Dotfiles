25e99c2c88e3e8c043cca64eb9f86799c8806711
Raitis Stengrevics <dagrevis@gmail.com>
2015-03-20 09:13:39 +0200

    Improve sound widget

--- sound_widget.py
+++ sound_widget.py
@@ -10,17 +10,20 @@ ICONS = {
 
 amixer_output = check_output(["amixer", "sget", "Master"]).decode("utf-8")
 
-volumes = re.findall(r"(\d+)\%", amixer_output)
+volumes = [int(x) for x in re.findall(r"(\d+)\%", amixer_output)]
 is_muted = re.search(r"\[off\]", amixer_output) is not None
 
-volume_left, volume_right = volumes
-
-if volume_left == volume_right:
-    output_volumes = "{}%".format(volume_left)
-else:
-    output_volumes = "L/R {}%/{}%".format(*volumes)
-
 volume_total = sum([int(x) for x in volumes])
+volumes_count = len(volumes)
+is_volumes_equal = volume_total // volumes_count == volumes[0]
+
+if is_volumes_equal:
+    output_volumes = "{}%".format(volumes[0])
+elif volumes_count == 2:
+    output_volumes = "L/R {}%/{}%".format(*volumes)
+else:
+    output_volumes = "/".join(volumes)
+
 if is_muted or volume_total == 0:
     output_icon = ICONS["fa-volume-off"]
 else:
