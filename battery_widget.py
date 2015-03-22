43e19c597b91c5bc5f93c922d228c0f50e01d76d
Raitis Stengrevics <dagrevis@gmail.com>
2015-03-22 23:44:19 +0200

    Remove thermal info from battery widget, add info about charging status

--- battery_widget.py
+++ battery_widget.py
@@ -1,13 +1,13 @@
 from subprocess import check_output
-from re import search, findall
+from re import search
 
 
-acpi_output = check_output(["acpi", "-bt"]).decode("utf-8")
+acpi_output = check_output(["acpi", "-b"]).decode("utf-8")
 
 percentage = search(r"(\d+)\%", acpi_output).group(1)
-thermals = findall(r"(\d+\.\d+) degrees", acpi_output)
+is_charging = search(r"Charging", acpi_output) is not None
 
-print("\uf0e7 {}% ({})".format(
-    percentage,
-    ", ".join(["{}C".format(x) for x in thermals])
+print("\uf0e7 {percentage}%{is_charging}".format(
+    percentage=percentage,
+    is_charging="+" if is_charging else "",
 ))
