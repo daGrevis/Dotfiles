3c07fa450a078e298405c0a2b4ffd5940eec7445
Raitis Stengrevics <dagrevis@gmail.com>
2015-04-11 01:06:58 +0300

    Improve monitor widget so it shows warmth as %

--- bspwm/Utils/monitor_widget.py
+++ bspwm/Utils/monitor_widget.py
@@ -7,8 +7,10 @@ ICONS = {
     "fa-desktop": "\uf108",
 }
 
+TEMPERATURE_MIN = 3500
+TEMPERATURE_MAX = 5500
+
 xbacklight_output = check_output(["xbacklight"]).decode("utf-8")
-brightness = Decimal(xbacklight_output).quantize(Decimal("1"))
 
 redshift_output = check_output([
     "redshift",
@@ -16,12 +18,27 @@ redshift_output = check_output([
     "57:24",
     "-p",
 ]).decode("utf-8")
-temperature = re.findall(r"(\d+)K", redshift_output)[0]
+temperature = Decimal(re.findall(r"(\d+)", redshift_output)[0])
 
-output = "{icon} {brightness}% ({temperature}K)".format(
-    icon=ICONS["fa-desktop"],
-    brightness=brightness,
-    temperature=temperature,
+output_icon = ICONS["fa-desktop"]
+
+output_brightness = Decimal(xbacklight_output).quantize(Decimal("1"))
+
+# 3500K -> 100%
+# 5000K -> 25%
+# 5500K -> 0%
+temperature_warmth = (
+    100 - ((temperature - TEMPERATURE_MIN) /
+           (TEMPERATURE_MAX - TEMPERATURE_MIN) * 100)
+)
+output_temperature = "{temperature_warmth}%w".format(
+    temperature_warmth=temperature_warmth,
+)
+
+output = "{icon} {brightness}% ({temperature})".format(
+    icon=output_icon,
+    brightness=output_brightness,
+    temperature=output_temperature,
 )
 
 print(output)
