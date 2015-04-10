4ff1479859c9c7405e791e7231f312cebc2fd92d
Raitis Stengrevics <dagrevis@gmail.com>
2015-04-10 21:47:03 +0300

    Add stow_everything.sh for easier "stowing"

---
+++ utils/Utils/stow_everything.sh
@@ -0,0 +1,35 @@
+declare -a pkgs=(
+    "ack"
+    "bspwm"
+    "coffeelint"
+    "dunst"
+    "editorconfig"
+    "flake8"
+    "git"
+    ".git"
+    "gtk"
+    "lein"
+    "mpv"
+    "pentadactyl"
+    "sxhkd"
+    "utils"
+    "vim"
+    "x"
+    "xdg-open"
+    "xfce4-terminal"
+    "zsh"
+)
+
+echo "Going to ~/Dotfiles"
+cd ~/Dotfiles
+
+echo
+for pkg in "${pkgs[@]}"
+do
+   stow -R "$pkg"
+   echo "(Re)stowing $pkg"
+done
+
+echo
+echo "Going back to..."
+cd -
