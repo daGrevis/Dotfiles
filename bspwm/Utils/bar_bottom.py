052c65f68e6f48720c84a66de093ceb238643baa
Raitis Stengrevics <dagrevis@gmail.com>
2015-09-24 23:07:14 +0300

    Improve bar_bottom

--- bspwm/Utils/bar_bottom.py
+++ bspwm/Utils/bar_bottom.py
@@ -4,7 +4,10 @@ import re
 from os import path
 from sys import argv, stdout
 
-from lemony import set_foreground_color, set_background_color, set_bold, set_line_color, set_underline, set_monitor
+from lemony import (
+    set_foreground_color, set_background_color, set_bold, set_line_color, set_underline,
+    set_monitor,
+)
 
 from widgets import COLORS, Widget, cache, notify_exception, debug
 
@@ -21,21 +24,7 @@ logger_handler.setFormatter(logger_formatter)
 logger.addHandler(logger_handler)
 
 
-MONITOR_PREFIXES = ("M", "m")
-DESKTOP_OCCUPIED_PREFIXES = ("O", "o")
-DESKTOP_FOCUSED_PREFIXES = ("F", "O", "U")
-DESKTOP_URGENT_PREFIXES = ("U", "u")
-DESKTOP_PREFIXES = (
-    "F",
-    "O",
-    "U",
-    "f",
-    "o",
-    "u",
-)
-LAYOUT_PREFIX = "L"
-
-SHORT_TITLE_MAPPING = {
+SHORT_WINDOW_NAME_MAPPING = {
     "firefox": [r"(.+ - )?Mozilla Firefox$", r".*Pentadactyl$"],
     "hipchat": [r"^HipChat$"],
     "mpv": [r".+ - mpv$"],
@@ -90,9 +79,9 @@ class WindowWidget(Widget):
     def render(self):
         w = self.window
 
-        short_title = get_short_title(w["title"])
+        short_name = get_short_window_name(w["window_name"])
 
-        text = " {} ".format(short_title)
+        text = " {} ".format(short_name)
 
         background_color = COLORS["01"]
         foreground_color = COLORS["05"]
@@ -103,54 +92,6 @@ class WindowWidget(Widget):
         text = set_foreground_color(text, foreground_color)
 
         return text
-
-
-def get_focused_window_id():
-    try:
-        output = subprocess.check_output([
-            "bspc query -w focused -W",
-        ], shell=True).decode("utf-8")
-    except subprocess.CalledProcessError:
-        return None
-    window_id = output.strip()
-
-    return window_id
-
-
-def get_focused_monitor_name():
-    output = subprocess.check_output([
-        "bspc query -m focused -M",
-    ], shell=True).decode("utf-8")
-    monitor_name = output.strip()
-
-    return monitor_name
-
-
-def get_window_ids_in_current_desktop():
-    output = subprocess.check_output([
-        "bspc query -d focused -W",
-    ], shell=True).decode("utf-8")
-    window_ids = output.split("\n")[:-1]
-
-    return window_ids
-
-
-def get_window_ids():
-    output = subprocess.check_output([
-        "bspc query -d focused -W",
-    ], shell=True).decode("utf-8")
-    window_ids = output.split("\n")[:-1]
-
-    return window_ids
-
-
-def get_titles(window_ids):
-    output = subprocess.check_output([
-        "xtitle {}".format(" ".join(window_ids)),
-    ], shell=True).decode("utf-8")
-    titles = output.split("\n")[:-1]
-
-    return titles
 
 
 def get_monitors(line):
@@ -190,7 +131,7 @@ def get_monitors(line):
         content = part[1:]
 
         # If it's a monitor...
-        if prefix in MONITOR_PREFIXES:
+        if prefix in "Mm":
             monitor = {
                 "monitor_name": content,
 
@@ -205,47 +146,120 @@ def get_monitors(line):
             monitors.append(monitor)
 
         # Adds the layout options to monitor....
-        if prefix == LAYOUT_PREFIX:
+        if prefix == "L":
             monitor["is_tiled"] = content == "T"
             monitor["is_monocle"] = content == "M"
 
         # If it's a desktop that belongs to monitor...
-        if prefix in DESKTOP_PREFIXES:
+        if prefix in "FOUfou":
             monitor["desktops"].append({
                 "desktop_name": content,
 
-                "is_focused": prefix in DESKTOP_FOCUSED_PREFIXES,
-                "is_occupied": prefix in DESKTOP_OCCUPIED_PREFIXES,
-                "is_urgent": prefix in DESKTOP_URGENT_PREFIXES,
+                "is_focused": prefix in "FOU",
+                "is_occupied": prefix in "Oo",
+                "is_urgent": prefix in "Uu",
             })
 
     return monitors
 
 
-def get_windows():
-    window_ids = get_window_ids_in_current_desktop()
-    focused_window_id = get_focused_window_id()
-    titles = get_titles(window_ids)
-    titles_by_window_id = dict(zip(window_ids, titles))
+def get_window_ids_in_monitor(monitor_id):
+    output = subprocess.check_output([
+        "bspc",
+        "query",
+        "-m",
+        "^{}".format(monitor_id),
+        "-W",
+    ]).decode("utf-8")
+    window_ids = output.split("\n")[:-1]
 
-    windows = [{"id": w,
-                "title": titles_by_window_id[w],
-                "is_focused": w == focused_window_id,
-                } for w in window_ids]
+    return window_ids
+
+
+def get_window_ids_in_focused_desktops():
+    output = subprocess.check_output([
+        "bspc",
+        "query",
+        "-d",
+        "focused",
+        "-W",
+    ]).decode("utf-8")
+    window_ids = output.split("\n")[:-1]
+
+    return window_ids
+
+
+def get_focused_window_id():
+    try:
+        output = subprocess.check_output([
+            "bspc",
+            "query",
+            "-w",
+            "focused",
+            "-W",
+        ]).decode("utf-8")
+    except subprocess.CalledProcessError:
+        return None
+    window_id = output.strip()
+
+    return window_id
+
+
+def get_window_names(window_ids):
+    output = subprocess.check_output(
+        ["xtitle"] + window_ids
+    ).decode("utf-8")
+    names = output.split("\n")[:-1]
+
+    return names
+
+
+def get_short_window_name(name):
+    for short_name, regexs in SHORT_WINDOW_NAME_MAPPING.items():
+        for regex in regexs:
+            if re.match(regex, name) is not None:
+                return short_name
+
+    return name
+
+
+def get_windows(monitor_id):
+    """
+    windows = [
+        {
+            window_id: int
+
+            window_name: str
+
+            is_focused: bool
+        },
+        ...
+    ]
+    """
+
+    window_ids_in_focused_desktops = get_window_ids_in_focused_desktops()
+    window_ids_in_monitor = get_window_ids_in_monitor(monitor_id)
+
+    # Can't use sets here because need to maintain order.
+    window_ids = [
+        window_id
+        for window_id
+        in window_ids_in_focused_desktops
+        if window_id in window_ids_in_monitor
+    ]
+
+    focused_window_id = get_focused_window_id()
+    window_id_to_window_names = dict(zip(window_ids, get_window_names(window_ids)))
+    windows = [{
+        "window_id": window_id,
+        "window_name": window_id_to_window_names[window_id],
+        "is_focused": window_id == focused_window_id,
+    } for window_id in window_ids]
 
     return windows
 
 
-def get_short_title(title):
-    for short_title, regexs in SHORT_TITLE_MAPPING.items():
-        for regex in regexs:
-            if re.match(regex, title) is not None:
-                return short_title
-
-    return title
-
-
-def render_monitor(monitor, windows):
+def render_to_monitor(monitor, windows):
     if monitor["monitor_id"] == 1:
         focused_color = COLORS["blue"]
     else:
@@ -295,10 +309,11 @@ def render_monitor(monitor, windows):
 
 
 def render(monitors):
-    windows = get_windows()
-
     for monitor in monitors:
-        render_monitor(monitor, windows)
+        render_to_monitor(
+            monitor,
+            get_windows(monitor["monitor_id"]),
+        )
 
 
 def main():
