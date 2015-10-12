99c23b405cceb3b10d8f885d6c549ecc0ac5d219
Raitis Stengrevics <dagrevis@gmail.com>
2015-10-12 20:04:08 +0300

    Fix and improve DiskUsageWidget

--- bspwm/Utils/bar_top.py
+++ bspwm/Utils/bar_top.py
@@ -473,37 +473,43 @@ class DiskUsageWidget(Widget):
         if paths is None:
             paths = [
                 "/",  # root
-                os_path.expanduser("~"),  # home dir
+                "/home/",  # home dir
             ]
 
         return map(psutil.disk_usage, paths)
+
+    def to_gb(self, b):
+        gb = b / 1024 / 1024 / 1024
+        return gb
 
     def render(self):
         disk_usages = self.get_disk_usages()
 
         text_parts = []
         for disk_usage in disk_usages:
-            total_gb = disk_usage.total / 1024 / 1024 / 1024
-            used_gb = disk_usage.used / 1024 / 1024 / 1024
-            free_gb = disk_usage.free / 1024 / 1024 / 1024
+            total = disk_usage.free + disk_usage.used
+            used = disk_usage.used
+            free = disk_usage.free
+            percentage = used / total * 100
 
             text = "{}/{} GB".format(
-                set_bold(round(used_gb)),
-                round(total_gb),
+                set_bold(round(self.to_gb(used))),
+                round(self.to_gb(total)),
             )
 
             is_critical = False
 
             # Numbers below are pretty random and should be adjusted manually.
-            is_storage_drive = total_gb >= 80
+            is_storage_drive = self.to_gb(total) >= 80
             if is_storage_drive:
-                if free_gb < 20:
+                if self.to_gb(free) < 20:
                     is_critical = True
             else:
                 if disk_usage.percent >= 90:
                     is_critical = True
 
             if is_critical:
+                text += " ({}%)".format(round(percentage, 2))
                 text = set_line_color(set_overline(text), COLORS["red"])
 
             text_parts.append(text)
