98447220f5a61a9f68a948b72333be108bd5c425
Raitis Stengrevics <dagrevis@gmail.com>
2015-11-16 16:58:41 +0200

    Add cron.py + minor improvements

--- utils/Utils/restart_wm.sh
+++ utils/Utils/restart_wm.sh
@@ -3,7 +3,7 @@
 killall sxhkd
 sxhkd &
 
-./Utils/keyboard_settings.sh
+keyboard_settings.sh
 
 killall xbanish
 xbanish &
@@ -17,13 +17,16 @@ killall compton
 compton &
 
 killall bar_top.sh
-~/Utils/bar_top.sh &
+bar_top.sh &
 
 killall bar_bottom.sh
-~/Utils/bar_bottom.sh &
+bar_bottom.sh &
 
 killall dunst
 dunst &
 
 killall stalonetray
 (sleep 2 && stalonetray) &
+
+kill -9 $(pgrep -f Utils/cron.py)
+start_cron.sh
