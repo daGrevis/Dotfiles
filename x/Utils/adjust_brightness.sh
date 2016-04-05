e2565c040068417f588dc00d61a96e7a313419ac
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-05 21:10:30 +0300

    Adjust brightness

--- x/Utils/adjust_brightness.sh
+++ x/Utils/adjust_brightness.sh
@@ -4,13 +4,13 @@ h=$(date "+%k")
 echo "Current hour: $h (0..23)"
 
 if ((h >= 23)); then
-    br=30
+    br=25
 elif ((h >= 20)); then
     br=50
 elif ((h >= 18 )); then
-    br=70
+    br=75
 elif ((h >= 10 )); then
-    br=90
+    br=100
 elif ((h >= 7 )); then
     br=50
 elif ((h >= 0 )); then
