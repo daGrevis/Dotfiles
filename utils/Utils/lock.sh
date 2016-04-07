6366f2c88e9df52e562488c7f313a359dd62c3e0
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-08 02:48:55 +0300

    Set urgency to low

--- utils/Utils/lock.sh
+++ utils/Utils/lock.sh
@@ -3,7 +3,7 @@
 # I don't want notification in screensaver.
 scrot /tmp/screenshot.jpg
 
-notify-send 'Locking screen' 'Right now'
+notify-send -u low 'Locking screen'
 
 convert -colorspace gray /tmp/screenshot.jpg -scale 25% -scale 400% /tmp/screensaver.png
 
