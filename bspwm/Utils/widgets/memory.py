c145c677682fc6cbc97d7dd12236531942be9a4e
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-08 00:46:26 +0300

    Auto set progress bar length in memory widget

--- bspwm/Utils/widgets/memory.py
+++ bspwm/Utils/widgets/memory.py
@@ -1,7 +1,7 @@
 import psutil
 from lemony import set_bold, set_overline, set_line_color, progress_bar
 
-from widgets import Widget, ICONS, COLORS
+from widgets import Widget, ICONS, COLORS, debug
 
 
 class MemoryWidget(Widget):
@@ -24,7 +24,12 @@ class MemoryWidget(Widget):
         if memory.percent >= 75:
             output = set_line_color(set_overline(output), COLORS["red"])
 
-        bar = progress_bar(memory.percent, 10)
+        # GB per step unless it's <= 2 GB.
+        bar_steps = round(total_mb / 1000)
+        if bar_steps <= 2:
+            bar_steps = 10
+
+        bar = progress_bar(memory.percent, bar_steps)
 
         output += " " + self.wrap_in_brackets(bar)
 
