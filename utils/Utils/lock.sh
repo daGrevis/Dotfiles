152feddc491d5d0c53048f854f5d329f27207b39
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-08 22:03:28 +0300

    Fix lock.sh not working

--- utils/Utils/lock.sh
+++ utils/Utils/lock.sh
@@ -5,6 +5,6 @@ scrot tmp/screenshot.jpg
 
 notify-send -u low 'Locking Screen'
 
-convert "tmp/screenshot.jpg" -level "0%,100%,0.6" -filter "Gaussian" -resize "20%" -define "filter:sigma=1.5" -resize "500.5%" -fill "black" "tmp/screensaver.jpg"
+convert "tmp/screenshot.jpg" -level "0%,100%,0.6" -filter "Gaussian" -resize "20%" -define "filter:sigma=1.5" -resize "500.5%" -fill "black" "tmp/screensaver.png"
 
 i3lock -n -e -u -i "tmp/screensaver.png"; echo $(date +"%s") > ~/tmp/session_ts
