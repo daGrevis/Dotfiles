5b4eea2bbfe29eafddfdeac08b6d6db94d605583
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-08 13:55:12 +0300

    Change screensaver

--- utils/Utils/lock.sh
+++ utils/Utils/lock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # I don't want notification in screensaver.
-scrot /tmp/screenshot.jpg
+scrot tmp/screenshot.jpg
 
 notify-send -u low 'Locking Screen'
 
-convert -colorspace gray /tmp/screenshot.jpg -scale 25% -scale 400% /tmp/screensaver.png
+convert "tmp/screenshot.jpg" -level "0%,100%,0.6" -filter "Gaussian" -resize "20%" -define "filter:sigma=1.5" -resize "500.5%" -fill "black" "tmp/screensaver.jpg"
 
-i3lock -n -e -u -i "/tmp/screensaver.png"; echo $(date +"%s") > ~/tmp/session_ts
+i3lock -n -e -u -i "tmp/screensaver.png"; echo $(date +"%s") > ~/tmp/session_ts
