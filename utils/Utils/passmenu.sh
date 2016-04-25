3de5a2c84a7d14e154dc1ef817fee05b5c7b8293
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-25 22:18:57 +0300

    Add fav.py to passmenu.sh

--- utils/Utils/passmenu.sh
+++ utils/Utils/passmenu.sh
@@ -2,16 +2,25 @@
 
 shopt -s nullglob globstar
 
+export FAVFILE=~/passmenu.fav
+
 password_files=( ~/.password-store/**/*.gpg )
 password_files=( "${password_files[@]##*/.password-store/}" )
 password_files=( "${password_files[@]%.gpg}" )
 
-dmenu_input=$(printf '%s\n' "${password_files[@]}")
-name=$(themenu.sh "$dmenu_input")
+dmenu_input=$(
+    fav.py ls --only-phrases;
+    printf '%s\n' "${password_files[@]}"
+)
+# Filter out unique lines without sorting.
+dmenu_input=$(echo "$dmenu_input" | awk '!x[$0]++')
 
-pass --clip "$name"
+phrase=$(themenu.sh "$dmenu_input")
+
+pass --clip "$phrase"
 rc=$?
 
-if [[ "$name" != "" ]] && [[ $rc -eq 0 ]]; then
-    notify-send -u low "Password Copied" "$name"
+if [[ "$phrase" != "" ]] && [[ $rc -eq 0 ]]; then
+    fav.py insert "$phrase"
+    notify-send -u low "Password Copied" "$phrase"
 fi;
