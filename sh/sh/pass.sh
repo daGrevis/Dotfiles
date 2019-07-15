7d768497d07c758aeac21c021c52338d2ff796a2
Raitis Stengrevics <dagrevis@gmail.com>
2019-07-15 22:37:38 +0300

    Don't clear the password after 45s when using pwc

--- sh/sh/pass.sh
+++ sh/sh/pass.sh
@@ -2,7 +2,10 @@
 
 # https://www.passwordstore.org/
 alias pwl='pass list'
-alias pwc='pass --clip'
 alias pws='pass show'
 alias pwe='pass edit'
 alias pwi='pass insert -m'
+
+pwc() {
+  pass show "$1" | pbcopy
+}
