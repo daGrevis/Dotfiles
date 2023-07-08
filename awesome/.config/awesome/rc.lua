951a7f642421ae40a4d3f9687d55f4f0bf3cc202
Raitis Stengrevics <dagrevis@gmail.com>
2023-07-08 22:51:17 +0300

    Remove tasklist from awesome leaving no UI at all

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -2,7 +2,6 @@ pcall(require, "luarocks.loader")
 
 local gears = require("gears")
 local awful = require("awful")
-local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
