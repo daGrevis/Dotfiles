bc74431b51109e85d7bf788fc20cda5ef7c02f3a
Raitis Stengrevics <dagrevis@gmail.com>
2023-07-08 22:45:29 +0300

    Fix font in awesome

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -39,7 +39,7 @@ bg3 = "#29394f"
 white = "#dfdfe0"
 
 beautiful.init({
-  font = "VictorMono Nerd Font 12",
+  font = "Recursive Mono Linear Static",
   bg_normal = bg0,
   fg_normal = fg1,
   bg_focus = bg3,
