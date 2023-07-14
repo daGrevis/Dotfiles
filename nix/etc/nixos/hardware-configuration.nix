91942635640f662caa186b0a277f9798e389c04e
Raitis Stengrevics <dagrevis@gmail.com>
2023-07-14 11:56:15 +0300

    Update hardware-configuration.nix

--- nix/etc/nixos/hardware-configuration.nix
+++ nix/etc/nixos/hardware-configuration.nix
@@ -6,17 +6,25 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a17c2ba5-57c5-4b59-a0e4-137b223fdec8";
+    { device = "/dev/disk/by-uuid/4c9cf748-9779-4349-aa92-6750b2722a7c";
       fsType = "ext4";
     };
 
   swapDevices = [ ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   virtualisation.virtualbox.guest.enable = true;
 }
