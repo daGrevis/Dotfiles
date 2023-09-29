77daf1293200e01e658f16131e28c3ee9ff7c0bb
Raitis Stengrevics <dagrevis@gmail.com>
2023-09-29 19:15:20 +0300

    Simplify "dps" to regular "docker ps"

--- sh/sh/docker.sh
+++ sh/sh/docker.sh
@@ -6,34 +6,7 @@ alias dlg='docker logs'
 dlgf() {
     docker logs -f "$@" 2>&1
 }
-# https://gist.github.com/GottZ/4a6c2af314d73cd8b71d
-dps() {
-  docker ps $@ --format "table{{ .Image }}\\t{{ .Names }}\\t{{ .Status }}\\t{{ .Ports }}" | awk '
-    NR % 2 == 0 {
-      printf "\033[0m";
-    }
-    NR % 2 == 1 {
-      printf "\033[1m";
-    }
-    NR == 1 {
-      PORTSPOS = index($0, "PORTS");
-      PORTS = "PORTS";
-      PORTSPADDING = "\n";
-      for(n = 1; n < PORTSPOS; n++)
-        PORTSPADDING = PORTSPADDING " ";
-    }
-    NR > 1 {
-      PORTS = substr($0, PORTSPOS);
-      gsub(/, /, PORTSPADDING, PORTS);
-    }
-    {
-      printf "%s%s\n", substr($0, 0, PORTSPOS - 1), PORTS;
-    }
-    END {
-      printf "\033[0m";
-    }
-  '
-}
+alias dps='docker ps'
 alias dpsa='docker ps -a'
 alias drm='docker rm -f'
 alias drmi='docker rmi'
