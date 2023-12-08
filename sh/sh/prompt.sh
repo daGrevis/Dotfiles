e46da6b33bed95c9d6825525b43c8d2113ca86d4
Raitis Stengrevics <dagrevis@gmail.com>
2023-12-08 14:13:14 +0200

    Refactor prompt.sh, set PROMPT in precmd

--- sh/sh/prompt.sh
+++ sh/sh/prompt.sh
@@ -3,18 +3,20 @@
 # Needed for function expansion to work in PROMPT.
 setopt PROMPT_SUBST
 
-return_status() {
-    if [ "$?" != '0' ]; then
+char_prompt() {
+    local exit_code="$1"
+
+    if [ "$exit_code" != '0' ]; then
         echo -n '%B%F{red}!%f%b'
     else
         echo -n '%B%F{green}>%f%b'
     fi
 }
 
-extra_status() {
+properties_prompt() {
     has_output=0
 
-    if [ "$(whoami)" = 'root' ]; then
+    if [ "$USER" = 'root' ]; then
         has_output=1
         echo -n '%B%F{red}[root]%f%b'
     fi
@@ -29,15 +31,15 @@ extra_status() {
     fi
 }
 
-git_prompt_info() {
+git_prompt() {
     local is_dirty=0
     local is_repo=1
 
     if git diff-files --quiet 2> /dev/null; then
         is_dirty=0
     else
-        local rc="$?"
-        if [ "$rc" = 128 ]; then
+        local exit_code="$?"
+        if [ "$exit_code" = 128 ]; then
             is_repo=0
         else
             is_dirty=1
@@ -46,7 +48,7 @@ git_prompt_info() {
 
     if [ "$is_repo" -eq 0 ]; then
         echo -n ''
-        return 1
+        return
     fi
 
     local branch
@@ -71,18 +73,18 @@ git_prompt_info() {
     echo -n "%B%F{blue}git[%f%F{white}$location%f%F{yellow}$dirty%f%F{blue}]%f%b "
 }
 
-preexec() {
-    timer=$(print -P '%D{%s%3.}')
-}
+exit_code_prompt() {
+    local exit_code="$1"
 
-precmd() {
-    statuscode="$?"
-    statusprompt=''
-    if [ "$statuscode" != '0' ]; then
-        statusprompt="%B%F{red}$statuscode%f%b "
+    if [ "$exit_code" != '0' ]; then
+        echo -n "%B%F{red}$exit_code%f%b "
+        return
     fi
 
-    timeprompt=''
+    echo -n ''
+}
+
+timing_prompt() {
     if [ "$timer" ]; then
         now=$(print -P '%D{%s%3.}')
         local d_ms=$((now - timer))
@@ -92,25 +94,38 @@ precmd() {
         local m=$(((d_s / 60) % 60))
         local h=$((d_s / 3600))
 
+        local duration=''
         if ((h > 0)); then
-            timeprompt="${h}h ${m}m ${s}s" # 1h 2m 3s
+            duration="${h}h ${m}m ${s}s" # 1h 2m 3s
         elif ((m > 0)); then
-            timeprompt="${m}m ${s}s" # 1m 2s
+            duration="${m}m ${s}s" # 1m 2s
         elif ((s > 0)); then
-            timeprompt="${s}.$(printf %03d $ms)s" # 1.234s
+            duration="${s}.$(printf %03d $ms)s" # 1.234s
         elif ((ms > 0)); then
-            timeprompt="${ms}ms" # 123ms
+            duration="${ms}ms" # 123ms
         fi
 
-        if [ "$timeprompt" ]; then
-            timeprompt="%B%F{black}${timeprompt} %f%b"
+        if [ "$duration" ]; then
+            echo -n "%B%F{black}${duration} %f%b"
+            return
         fi;
 
         unset timer
     fi
 
-    export RPROMPT="${statusprompt}${timeprompt}"
+    echo -n ''
 }
 
-# shellcheck disable=SC2016
-export PROMPT='$(return_status) $(extra_status)%F{cyan}%c%f $(git_prompt_info)'
+preexec() {
+    timer=$(print -P '%D{%s%3.}')
+}
+
+precmd() {
+    exit_code="$?"
+
+    PROMPT="$(char_prompt $exit_code) $(properties_prompt)%F{cyan}%c%f $(git_prompt)"
+    export PROMPT
+
+    RPROMPT="$(exit_code_prompt $exit_code)$(timing_prompt)"
+    export RPROMPT
+}
