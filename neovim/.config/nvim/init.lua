a346a38c7078f88194140d5298c0f2dea5632cc5
Raitis Stengrevics <dagrevis@gmail.com>
2025-02-26 18:36:19 +0200

    Change git blame mapping to <leader>b

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -323,7 +323,7 @@ require('lazy').setup {
         map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
         map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
         map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+        map('n', '<leader>b', gitsigns.blame_line, { desc = 'git [b]lame line' })
         map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
         map('n', '<leader>hD', function()
           gitsigns.diffthis '@'
