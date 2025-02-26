eda2b35af97f07d4834c59ce7a6915fa4259c388
Raitis Stengrevics <dagrevis@gmail.com>
2025-02-26 18:44:36 +0200

    Auto-close neo-tree when opening file

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -979,6 +979,14 @@ require('lazy').setup {
       window = {
         width = 60,
       },
+      event_handlers = {
+        {
+          event = 'file_open_requested',
+          handler = function()
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
     },
   },
 
