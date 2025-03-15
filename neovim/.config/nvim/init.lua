4910b3c1694129457351ea72ead566a64b4b6a1b
Raitis Stengrevics <dagrevis@gmail.com>
2025-03-15 11:02:03 +0200

    Properly remove nvim-cmp leftovers

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -458,9 +458,6 @@ require('lazy').setup {
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
-
       'saghen/blink.cmp',
     },
     config = function()
@@ -615,7 +612,6 @@ require('lazy').setup {
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -1035,12 +1031,7 @@ require('lazy').setup {
   { -- Auto-close delimiters.
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/nvim-cmp',
-    },
-    config = function()
-      require('nvim-autopairs').setup {}
-    end,
+    config = true,
   },
 
   { -- Moves the argument to left or right side.
