3818b070974b2164000ffd38b58ac59597e6862d
Raitis Stengrevics <dagrevis@gmail.com>
2026-06-02 10:58:09 +0300

    Update telescope to latest master

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -501,7 +501,7 @@ require('lazy').setup {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
